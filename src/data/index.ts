7f5642fdcb03e285fdaf7819e86c399ab4353812
TiagoRoccatagliata <tiagoroccatagliata@gmail.com>
2025-11-05 23:08:03 -0300

    Init

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+import type { Currency } from "../types";
+
+export const currencies: Currency[] = [
+    { code: 'USD', name: 'Dolar de Estados Unidos' },
+    { code: 'ARG', name: 'Peso Argentino' },
+    { code: 'EUR', name: 'Euro' },
+]
